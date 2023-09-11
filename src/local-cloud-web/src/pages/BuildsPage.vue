2fe7fe0d602eeafe816b055e6c2ed50cbc9c10ec
u11703 <nkisselev@alfabank.kz>
2023-09-11 19:50:38 +0600

    added web project

---
+++ src/local-cloud-web/src/pages/BuildsPage.vue
@@ -0,0 +1,43 @@
+<script setup>
+const headers = [
+  {
+    label: 'name',
+    field: 'name',
+    name: 'name',
+  },
+  {
+    label: 'actions',
+    field: 'actions',
+    name: 'actions',
+  },
+]
+const data = [
+  {
+    name: 'project 1',
+  },
+  {
+    name: 'project 2',
+  },
+]
+</script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <q-table
+      :columns="headers"
+      :rows="data"
+      row-key="name"
+    >
+      <template v-slot:body-cell-actions="props">
+        <td :props="props">
+          <q-btn>Rebuild & Publish</q-btn>
+        </td>
+      </template>
+    </q-table>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/local-cloud-web/src/router/routes.js
@@ -0,0 +1,26 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/builds',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/BuildsPage.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  },
+]
+
+export default routes
